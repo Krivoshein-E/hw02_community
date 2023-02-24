31f036fe4ac54cdc9ef6766975ebc35dd03221f1
Кривошеин Евгений <Krivoshein.eo@yandex.ru>
2023-02-24 19:42:48 +0500

    urls.py изменил name

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,6 +5,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+def __str__(self):
+        return self.title
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -15,18 +23,8 @@ class Post(models.Model):
     )
 
     group = models.ForeignKey(
-        'Group',
+        Group,
         blank=True,
         null=True,
         on_delete=models.CASCADE
         )
-
-
-class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
-
-
-    def __str__(self):
-        return self.title
