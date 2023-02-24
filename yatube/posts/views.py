06ec93327e31b784a7998bab86ea681cf566c35a
Кривошеин Евгений <Krivoshein.eo@yandex.ru>
2023-02-24 19:19:47 +0500

    все изменения

---
+++ yatube/posts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+
+# Create your views here.
+def index(request):
+    template = 'posts/index.html'
+
+    title = 'Главная страница проекта YaTube'
+    text = 'Это главная страница проекта YaTube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts' : posts,
+        'title' : title,
+        'text' : text
+        }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+
+    text = 'Здесь будет информация о группах проекта YaTube'
+    context = {
+        'group' : group,
+        'posts': posts,
+        'text' : text
+        }
+    return render(request, template, context)
